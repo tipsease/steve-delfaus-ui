7e74f7401bd33d487798f8a9cdd70c2c3683383f
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-05 13:14:51 -0500

    replace placeholder text, fix mid animation
    Prior to this change, the middle-content features weren't flowing
    properly when text content of varying lengths was added.
    
    Firefox also kept throwing that error from lack of setting the character
    encoding on the html.
    
    There were also debug messages still appearing in the console.
    
    This change adds the missing character encoding tag, adds a 50% width on
    features at the desktop breakpoint, and removes the debug output. It
    also adds more realistic text.

--- components/Feature/Feature.js
+++ components/Feature/Feature.js
@@ -4,19 +4,12 @@ class Feature {
     this.element.addEventListener('click', () => this.read());
   }
 
+  // fade each feature out on click, also take out .middle-content if none left
   read() {
-    TweenMax.to(this.element, 1, { opacity: 0, onComplete: () => {
+    TweenMax.to(this.element, 0.2, { opacity: 0, onComplete: () => {
       this.element.style.display = 'none'
-      const features = Array.from(document.querySelectorAll('.feature'), (f) => {
-        const computedStyle = window.getComputedStyle(f);
-        console.log(computedStyle);
-        if(computedStyle.display === 'none') {
-          return false;
-        } else {
-          return true;
-        }
-      });
-      if(features.length - 1 <= 0) {
+      const features = Array.from(document.querySelectorAll('.feature')).filter(f => f.style.display !== 'none');
+      if(features.length <= 0) {
         document.querySelector('.middle-content').style.display = 'none';
       }
     }});
