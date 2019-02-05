38332aff00528b129bd5305465b16ce18a7beff6
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-05 10:50:17 -0500

    add fixed background banner image
    The template had one of these but it took a while to find an image that
    would keep the text readable.
    
    Ended up editing the colors on this banner image, and giving the tag
    line a darker background with reduced opacity.

--- components/Feature/Feature.js
+++ components/Feature/Feature.js
@@ -5,10 +5,23 @@ class Feature {
   }
 
   read() {
-    // this.element.style.display = 'none';
-    //
-    TweenMax.to(this.element, 1, { opacity: 0, onComplete: () => this.element.style.display = 'none'});
+    TweenMax.to(this.element, 1, { opacity: 0, onComplete: () => {
+      this.element.style.display = 'none'
+      const features = Array.from(document.querySelectorAll('.feature'), (f) => {
+        const computedStyle = window.getComputedStyle(f);
+        console.log(computedStyle);
+        if(computedStyle.display === 'none') {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      if(features.length - 1 <= 0) {
+        document.querySelector('.middle-content').style.display = 'none';
+      }
+    }});
+
   }
 }
 
-let features = Array.from(document.querySelectorAll(".feature")).map(f => new Feature(f));
+let features = Array.from(document.querySelectorAll('.feature')).map(f => new Feature(f));
