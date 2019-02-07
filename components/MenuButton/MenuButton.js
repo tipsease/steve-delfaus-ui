72bd8e5f3427f7c176a94885f151837cc668e43c
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-07 16:55:16 -0500

    refactor MenuButton.js
    Prior to this change, click behavior didn't exactly match html5up ( a
    click outside the menu should have closed the slide out )
    
    This change adds a click listener when the menu's open and refactors the
    code a bit.

--- components/MenuButton/MenuButton.js
+++ components/MenuButton/MenuButton.js
@@ -4,18 +4,30 @@ class MenuButton {
     this.shown = false;
     this.menu = document.querySelector('.nav-panel');
     this.slideWrapper = document.querySelector('.slide-wrapper');
-    this.element.addEventListener('click', () => this.toggleMenu());
+    this.element.addEventListener('click', e => this.toggleMenu(e));
+    this.hideMenu = this.hideMenu.bind(this);
   }
 
-  toggleMenu() {
+  hideMenu() {
+    this.menu.classList.add('menu-hidden');
+    this.slideWrapper.classList.add('menu-hidden');
+    this.shown = false;
+  }
+
+  showMenu() {
+    this.menu.classList.remove('menu-hidden');
+    this.slideWrapper.classList.remove('menu-hidden');
+    this.shown = true;
+  }
+
+  toggleMenu(e) {
+    e.stopPropagation();
     if(this.shown) {
-      this.menu.classList.add('menu-hidden');
-      this.slideWrapper.classList.add('menu-hidden');
-      this.shown = false;
+      this.slideWrapper.removeEventListener('click', this.hideMenu);
+      this.hideMenu();
     } else {
-      this.menu.classList.remove('menu-hidden');
-      this.slideWrapper.classList.remove('menu-hidden');
-      this.shown = true;
+      this.slideWrapper.addEventListener('click', this.hideMenu);
+      this.showMenu();
     }
   }
 }
